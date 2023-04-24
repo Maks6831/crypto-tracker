82279a73b2cb2fd0433cd1c8f89e448659e62810
Maks6831 <ma.khan6971@gmail.com>
2023-04-25 00:55:04 +0100

    search now displaying onto page

---
+++ src/components/Searchitem.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Searchitem = (props) => {
+  return (
+    <div>{props.name}</div>
+  )
+}
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -4,13 +4,14 @@ import { useNavigate } from 'react-router-dom';
 import { db } from '../Firebase';
 import { Trendcard } from '../components/Trendcard';
 import '../styles/Dashboard.css'
+import { Searchitem } from '../components/Searchitem';
 
 export const Dashboard = () => {
   const [displayName, setDisplayName] = useState('');
   const { logout, currentUser } = useAuth();
   const [trending, setTrending] = useState([]);
   const [query, setQuery] = useState('');
-  const [data, setData] = useState('');
+  const [data, setData] = useState([]);
   const history = useNavigate();
   const redirect = (path) => {
     history(path)
@@ -33,9 +34,7 @@ export const Dashboard = () => {
 
   const handleQuery = (e) =>{
     e.preventDefault()
-    fetch('https://api.coingecko.com/api/v3/search?query=' + query).then(res => res.json()).then(data=> console.log(data))
-
-
+    fetch('https://api.coingecko.com/api/v3/search?query=' + query).then(res => res.json()).then(data=> setData(data.coins))
   }
 
 
@@ -71,6 +70,14 @@ export const Dashboard = () => {
         <input type="search" name='query' placeholder='search' className='form-input' onChange={queryChange}/>
           <button onClick={handleQuery}>search</button>
       </form>
+    </div>
+    <div>
+      {data && data.map((item) => (
+        <Searchitem
+        name={item.name}
+         />
+      ))}
+      
     </div>
     
     
